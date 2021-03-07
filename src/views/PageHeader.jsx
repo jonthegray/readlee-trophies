c0ac91394fbf26abef4395de9689a8e02f31d080
jonthegray <80072347+jonthegray@users.noreply.github.com>
2021-03-06 21:42:10 -0500

    Adding font-awesome

--- src/views/PageHeader.jsx
+++ src/views/PageHeader.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 const PageHeader = (props) => {
   return <header>
     <img alt="Readlee logo" src="/Readlee+Logo+Red.png" />
+    <FontAwesomeIcon icon={faUserCircle} />
   </header>
 };
 
